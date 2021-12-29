c21d6c6b74a8c6cc4edfbf03fb07c1f768ebe1fd
shelly goman <shellygoman123@gmail.com>
2021-12-29 22:48:25 +0200

    fix

--- photomixerGUI/obj/Debug/net5.0-windows/App.g.cs
+++ photomixerGUI/obj/Debug/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace photomixerGUI {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace photomixerGUI {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
         public static void Main() {
             photomixerGUI.App app = new photomixerGUI.App();
             app.InitializeComponent();
