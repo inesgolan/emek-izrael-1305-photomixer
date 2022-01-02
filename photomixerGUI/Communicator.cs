127d2942e5c98a9eb23d8287a006b7afe60d8301
Ines Golan <noagolan1212@gmail.com>
2022-01-02 19:23:34 +0200

    added full path of exe and moved exe to GUI debug folder

--- photomixerGUI/Communicator.cs
+++ photomixerGUI/Communicator.cs
@@ -19,17 +19,16 @@ namespace photomixerGUI
         public void sendObjectRecognizeMsg(string objectPath, string savePath)
         {
             string exe_params = "100 " + objectPath + " " + savePath;
-            Console.WriteLine(exe_params);
-            string exe_full_path = Path.Combine("C:\\Users\\Ines Noa Golan\\source\\repos\\emek-izrael-1305-photomixer\\photomixer", "photomixer.exe");
-            Process proc = System.Diagnostics.Process.Start(exe_full_path, exe_params);
+            string path = Path.GetFullPath("photomixer.exe");
+            Process proc = System.Diagnostics.Process.Start(path, exe_params);
         }
 
 
         public void sendPasteObjectMsg(string objectPath, string backgroundPath, string savePath, int x, int y)
         {
             string exe_params = "200 " + objectPath + " " + backgroundPath + " " + savePath + " " + Convert.ToChar(x) + " " + Convert.ToChar(y);
-            string exe_full_path = Path.Combine("C:\\Users\\משתמש\\emek-izrael-1305-photomixer\\photomixer\\x64\\Debug", "photomixer.exe");
-            Process proc = System.Diagnostics.Process.Start(exe_full_path, exe_params);
+            string path = Path.GetFullPath("photomixer.exe");
+            Process proc = System.Diagnostics.Process.Start(path, exe_params);
         }
     }
 }
