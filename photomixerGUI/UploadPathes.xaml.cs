503cd0054ab2c42a2553d88d75b04854165db204
shelly goman <shellygoman123@gmail.com>
2022-01-08 16:28:08 +0200

    added the path checking - but still need to check thepath error

--- photomixerGUI/UploadPathes.xaml.cs
+++ photomixerGUI/UploadPathes.xaml.cs
@@ -1,25 +1,78 @@
 ﻿using System.Windows;
+using System.IO;
+using System.Windows.Media;
 
 namespace photomixerGUI
 {
     // user enters images pathes and this class checks the paths
     public partial class UploadPathes : Window
     {
+        private const int ENDING = 4;
         private Communicator communicator = new Communicator();
 
         public UploadPathes()
         {
             InitializeComponent();
-            string imagePath = "\"C:/Users/Ines Noa Golan/source/repos/emek-izrael-1305-photomixer/photomixer/images/bear2.jpg\"";
-            string savePath = "\"C:/Users/Ines Noa Golan/source/repos/emek-izrael-1305-photomixer/photomixer/images/here.png\"";
-            communicator.sendObjectRecognizeMsg(imagePath, savePath);
+            this.communicator = communicator;
         }
+
 
         private void objectDetection(object sender, RoutedEventArgs e)
         {
-            checkMatte check = new checkMatte("C:/Users/Ines Noa Golan/source/repos/emek-izrael-1305-photomixer/photomixer/images/bear2.jpg");
-            check.Show();
-            Close();
+            if (!isPathValid(objectPath.Text) || !isPathValid(backgroundPath.Text))
+            {
+                ErrorMsg.Text = "Error - wrong path - try again.";
+                ErrorMsg.Width = 60;
+                ErrorMsg.Height = 60;
+                ErrorMsg.Foreground = Brushes.DarkRed;
+                ErrorMsg.HorizontalAlignment = HorizontalAlignment.Left;
+                ErrorMsg.TextWrapping = TextWrapping.Wrap;
+            }
+            else
+            {
+                communicator.sendObjectRecognizeMsg(objectPath.Text, backgroundPath.Text);
+
+                checkMatte check = new checkMatte(objectPath.Text);
+                check.Show();
+                Close();
+            }
+        }
+
+        /*
+        This function will check if the path is valid
+        input:string path
+        output: bool
+        */
+        private bool isPathValid(string path)
+        {
+            string ending = "";
+            string localPath = path;
+
+            if (path.Length > ENDING) 
+            {
+                //C:\Users\משתמש\emek-izrael-1305-photomixer\photomixer\images/bear2.jpg
+                //C:\Users\משתמש\emek-izrael-1305-photomixer\photomixer\images/background.jpg
+
+                int startIndex = path.Length - ENDING;
+                int finishIndex = path.Length - 1;
+                int localPathlen = localPath.Length;
+
+                ending = path.Substring(startIndex, finishIndex - startIndex + 1);
+
+            }
+            else
+            {
+                return false;
+            }
+
+
+            bool fileExist = File.Exists(path);
+            if (fileExist && (".jpg" == ending || ".png" == ending)) //can open the file and it's a picture
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
