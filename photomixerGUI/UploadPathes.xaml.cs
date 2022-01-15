020d6a5e6c29e83920ca90e1c7b7fd0eddf8e14a
Ines Golan <noagolan1212@gmail.com>
2022-01-15 20:51:44 +0200

    display only the background

--- photomixerGUI/UploadPathes.xaml.cs
+++ photomixerGUI/UploadPathes.xaml.cs
@@ -8,7 +8,7 @@ namespace photomixerGUI
     // user enters images pathes and this class checks the paths
     public partial class UploadPathes : Window
     {
-        private const int SIZE = 10;
+        private const int SIZE = 11;
         private const int ENDING = 4;
 
         private Communicator communicator = new Communicator();
@@ -64,11 +64,13 @@ namespace photomixerGUI
                     //checkMatte check = new checkMatte(objectPath.Text);
                     //check.Show();
 
-                    imagesPathes[imagesCounter] = imagePath;
                     imagesCounter++;
-
                     string savePath = "objectImage" + imagesCounter.ToString() + ".png";
                     communicator.sendObjectRecognizeMsg(imagePath, savePath);
+
+                    //add the save path to the array
+                    imagesPathes[imagesCounter-1] = savePath;
+
                 }
             }
         }
@@ -86,8 +88,11 @@ namespace photomixerGUI
             {
                 ErrorMsg.Text = "ok";
 
-                //editImage edit = new editImage();
-                //edit.Show();
+                //put the background path in the last cell in the array
+                imagesPathes[imagesCounter] = pathBackground;
+
+                Edit edit = new Edit(imagesPathes, imagesCounter);
+                edit.Show();
                 Close();
             }
         }
