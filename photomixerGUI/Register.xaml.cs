5a3cd207801f183e0f9d0727829c19e09dab3d70
shelly goman <shellygoman123@gmail.com>
2022-01-31 13:09:04 +0200

    added the sign up screen

---
+++ photomixerGUI/Register.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace photomixerGUI
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow goToMainWindow = new MainWindow();
+            goToMainWindow.Show();
+            Close();
+        }
+
+        private void signUp_Button(object sender, RoutedEventArgs e)
+        {
+            Communicator.registerMsg(Username.Text, Password.Password, Mail.Text);
+            MainWindow goToMainWindow = new MainWindow();
+            goToMainWindow.Show();
+            Close();
+        }
+    }
+}
