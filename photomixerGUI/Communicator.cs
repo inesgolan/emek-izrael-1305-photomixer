d32e547f00574c30bfed25e27c8c8a05f7a4c4eb
shelly goman <shellygoman123@gmail.com>
2022-02-03 13:14:43 +0200

    changes the way that the picture after resize will appear on the screen

--- photomixerGUI/Communicator.cs
+++ photomixerGUI/Communicator.cs
@@ -54,6 +54,7 @@ namespace photomixerGUI
             string exe_params = "300 " + objectPath;
             string path = Path.GetFullPath("photomixer.exe");
             Process proc = System.Diagnostics.Process.Start(path, exe_params);
+            proc.WaitForExit();
         }
 
         public static void resizeObjectSmallMsg(string objectPath)
--- photomixerGUI/Resize.xaml.cs
+++ photomixerGUI/Resize.xaml.cs
@@ -13,7 +13,6 @@ namespace photomixerGUI
     public partial class Resize
     {
         private static string objectPath;
-        private int flag;
 
         // c'tors
         public Resize()
@@ -31,19 +30,13 @@ namespace photomixerGUI
         // make the picture size larger
         private void biggerButton_Click(object sender, RoutedEventArgs e)
         {
-            if (flag == 0)
-            {
-                Communicator.resizeObjectBigMsg(objectPath);
-            }
+            Communicator.resizeObjectBigMsg(objectPath);  
         }
 
         // make the picture size smaller
         private void smallerButton_Click(object sender, RoutedEventArgs e)
         {
-            if (flag == 0)
-            {
-                Communicator.resizeObjectSmallMsg(objectPath);
-            }
+            Communicator.resizeObjectSmallMsg(objectPath);
         }
 
         // back to upload pathes screen
@@ -55,14 +48,8 @@ namespace photomixerGUI
         // show the result of the size change
         private void resultButton_Click(object sender, RoutedEventArgs e)
         {
-            ObjectImage.Source = new BitmapImage(new Uri(objectPath));
-            string message = "Press on Back button.";
-            string caption = "U can't resize the picture anymore!";
-            MessageBoxButton buttons = MessageBoxButton.OK;
-
-            // Displays the MessageBox.
-            MessageBoxResult result = MessageBox.Show(message, caption, buttons);
-            flag = 1;
+            showPicture g = new showPicture(objectPath);
+            g.Show();
         }
     }
 }
