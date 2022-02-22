56e40c061d262251aa7f970dfcdf93620f65cf15
shelly goman <shellygoman123@gmail.com>
2022-02-22 14:36:35 +0200

    finished the last 2 decryption functions and check them, but the decryption does not work currectly

--- photomixer/encryption.py
+++ photomixer/encryption.py
@@ -25,6 +25,24 @@ sbox = [
    0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
    0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16]
+   
+sboxInv = [
+        0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+        0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+        0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+        0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+        0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+        0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+        0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+        0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+        0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+        0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+        0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+        0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+        0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+        0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+        0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+        0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d]
 
 keySchedule = [0x1,0x2,0x4,0x8,0x10,0x20,0x40,0x80,0x1B,0x36]
 
@@ -38,6 +56,13 @@ def subBytes(matrix):
     for row in range(SIZE):
         for column in range(SIZE):
             matrix[row][column] = hex(sbox[int(matrix[row][column], BASE)])
+            
+    return matrix
+	
+def inverseSubBytes(matrix):
+    for row in range(SIZE):
+        for column in range(SIZE):
+            matrix[row][column] = hex(sboxInv[int(matrix[row][column], BASE)])
             
     return matrix
 
@@ -80,7 +105,32 @@ def shiftRows(matrix):
     for row in range(3):
         newMatrix[row+1][3] = matrix[row][3]
 
-    return(newMatrix)   
+    return(newMatrix)  
+
+
+def inverseShiftRows(matrix):
+    newMatrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    
+    for row in range(SIZE):
+        newMatrix[row][0] = matrix[row][0]
+		
+    newMatrix[0][1] = matrix[3][1]
+	
+    for row in range(4):
+        newMatrix[row][1] = matrix[row-1][1]
+		
+		
+    for row in range(2):
+        newMatrix[row][2] = matrix[row-2][2]
+        newMatrix[row-2][2] = matrix[row][2]
+		
+    
+    newMatrix[3][3] = matrix[0][3]  
+    for row in range(4):
+        newMatrix[row-1][3] = matrix[row][3]
+	
+    #print(newMatrix)	
+    return(newMatrix) 
 
 
 '''
@@ -155,9 +205,9 @@ def inverseMixColumn(matrix):
     temp = copy(newMatrix)
     for times in range(SIZE):
         newMatrix[i] = galoisMult(temp[i],14) ^ galoisMult(temp[i+1],11) ^ galoisMult(temp[i+2],13) ^ galoisMult(temp[i+3],9)
-        newMatrix[i+1] = galoisMult(temp[i+1],14) ^ galoisMult(temp[i],11) ^ galoisMult(temp[i+3],13) ^ galoisMult(temp[i+2],9)
-        newMatrix[i+2] = galoisMult(temp[i+2],14) ^ galoisMult(temp[i+1],11) ^ galoisMult(temp[i],13) ^ galoisMult(temp[i+3],9)
-        newMatrix[i+3] = galoisMult(temp[i+3],14) ^ galoisMult(temp[i+2],11) ^ galoisMult(temp[i+1],13) ^ galoisMult(temp[i],9)
+        newMatrix[i+1] = galoisMult(temp[i+1],14) ^ galoisMult(temp[i],9) ^ galoisMult(temp[i+3],13) ^ galoisMult(temp[i+2],11)
+        newMatrix[i+2] = galoisMult(temp[i+2],14) ^ galoisMult(temp[i+1],9) ^ galoisMult(temp[i],13) ^ galoisMult(temp[i+3],11)
+        newMatrix[i+3] = galoisMult(temp[i+3],14) ^ galoisMult(temp[i+2],9) ^ galoisMult(temp[i+1],13) ^ galoisMult(temp[i],11)
         i += SIZE
            
     #convert from int to hex
@@ -177,7 +227,6 @@ Output: encryted matrix
 '''
 def addRoundKey(key, matrix):
     newMatrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    
     #add the key to the matrix
     for row in range(SIZE):
         for column in range(SIZE):
@@ -239,15 +288,15 @@ Output: encryted matrix
 def encryptionForEachPart(key, HexArray):
     matrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]] 
     arr = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    
+
     #convert the matrix from string to hex
     i = 0   
     for row in range(SIZE):
         for column in range(SIZE):
             matrix[row][column] = hex(HexArray[i])
             i += 1
-    
-    print ("before: ", matrix)
+			   
+    #print ("before111: ", matrix)
     matrix = addRoundKey(key, matrix)
     
     for round in range(ROUNDS):
@@ -280,7 +329,7 @@ Output: encryted matrix
 def encryption(key, HexArray):
     hexKey = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]] 
     i = 0
-    
+
     #convert the key from string to hex
     for row in range(SIZE):
         for column in range(SIZE):
@@ -304,35 +353,39 @@ def decryptionForEachPart(key, HexArray):
     matrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]] 
     arr = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     
+    #print(HexArray)
     #convert the matrix from string to hex
     i = 0   
     for row in range(SIZE):
         for column in range(SIZE):
             matrix[row][column] = hex(HexArray[i])
             i += 1
-    
-    print ("before: ", matrix)
-    matrix = addRoundKey(key, matrix)
-    matrix = inverseShiftRows(matrix)
-    matrix = inverseSubBytes(matrix)  
-    
+
+
+    matrix = addRoundKey(key, matrix)  
+    print(matrix)	
     for round in range(ROUNDS):
+        matrix = inverseShiftRows(matrix) 
+        matrix = inverseSubBytes(matrix)
+        matrix = inverseMixColumn(matrix)		
         key = roundKey(key)
         matrix = addRoundKey(key, matrix)
-        matrix = inverseMixColumn(matrix)
-        matrix = inverseShiftRows(matrix)
-        matrix = inverseSubBytes(matrix)        
+        print(matrix)
+        #print(matrix)		
      
+    matrix = inverseShiftRows(matrix)
+    matrix = inverseSubBytes(matrix)
     key = roundKey(key)
     matrix = addRoundKey(key, matrix)
     
+	
     #put the matrix in an array
     i = 0   
     for row in range(SIZE):
         for column in range(SIZE):
             arr[i] = matrix[row][column]
             i += 1
-    
+
     return (arr)
 
 '''
@@ -343,7 +396,7 @@ Output: decryted matrix
 def decryption(key, HexArray):
     hexKey = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]] 
     i = 0
-    
+
     #convert the key from string to hex
     for row in range(SIZE):
         for column in range(SIZE):
@@ -352,7 +405,7 @@ def decryption(key, HexArray):
 
     #turn the array to a two dimensions array
     [HexArray[index:index+16] for index in range(0,len(HexArray),16)]
-    
+
     HexArray = decryptionForEachPart(hexKey, HexArray)         
             
     return HexArray
@@ -363,6 +416,17 @@ def decryption(key, HexArray):
 # content = f.read()
 # print(binascii.hexlify(content))
 matrix = [0x54,0x77,0x6F,0x20,0x4F,0x6E,0x65,0x20,0x4E,0x69,0x6E,0x65,0x20,0x54,0x77,0x6F]
+matrix2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
 key = "Thats my Kung Fu" #needs to be 16 chars
-matrix = decryption(key, matrix)
-print("after: ", matrix)
+
+matrix = encryption(key, matrix)
+print("encryption: ", matrix)
+
+# hex string to hex
+for i in range(16):
+    matrix2[i] = int(matrix[i], 16)
+		
+count = 0
+
+matrix2 = decryption(key, matrix2)
+print("decryption: ", matrix2)
