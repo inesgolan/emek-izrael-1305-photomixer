a2cb32b115a0f01c95bb86332c1e1351cba637f0
shelly goman <shellygoman123@gmail.com>
2022-03-09 09:55:26 +0200

    finish the image sending

--- photomixer/sendMail.py
+++ photomixer/sendMail.py
@@ -1,18 +1,19 @@
 import os
+import sys
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 
-def SendMail(ImgFileName):
+def SendMail(ImgFileName, toMail):
 
     with open(ImgFileName, 'rb') as f:
         img_data = f.read()
 
     msg = MIMEMultipart()
-    msg['Subject'] = 'Hey'
+    msg['Subject'] = 'Your picture - PHOTOMIXER'
     msg['From'] = 'm2k21ivs@gmail.com'
-    msg['To'] = 'shellygoman123@gmail.com'
+    msg['To'] = str(toMail)
 
     text = MIMEText("test")
     msg.attach(text)
@@ -24,22 +25,18 @@ def SendMail(ImgFileName):
     s.starttls()
     s.ehlo()
     s.login('m2k21ivs@gmail.com', 'maabada123')
-    s.sendmail('m2k21ivs@gmail.com', 'shellygoman123@gmail.com', msg.as_string())
+    s.sendmail('m2k21ivs@gmail.com', str(toMail), msg.as_string())
     s.quit()
 
-
-
-
-
-
-	
-    # server = smtplib.SMTP(host='smtp.gmail.com', port=587)
-    # server.ehlo()
-    # server.starttls()
-    # server.ehlo()
-    # server.login('m2k21ivs@gmail.com', 'maabada123')
-    # server.sendmail('m2k21ivs@gmail.com', 'shellygoman123@gmail.com', 'hey')
-    # server.quit()
     print("ok")
 	
-SendMail("images/b.jpg")
+# none imagePath toMail 
+# images/b.jpg
+#shellygoman123@gmail.com
+def main():
+	
+    SendMail(sys.argv[1], sys.argv[2])
+	
+if __name__ == "__main__":
+    main()
+
